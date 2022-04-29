ef889303cf052dc1816e1a94a1e04e21f703f2b6
Домогатский Алексей <domogatskiy.a@yandex.ru>
2022-04-29 12:45:33 +0300

    feat: count-cats, drream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if(!Array.isArray(members)){return false;};
+  let charMemeber = [];
+  members.forEach(element => {
+      if(typeof element === 'string' && element.trimStart().length > 0 ){
+        charMemeber.push(element.trimStart()[0].toUpperCase());
+      }
+      
+  }); 
+  return charMemeber.sort().join(''); 
 }
 
 module.exports = {
   createDreamTeam
 };
+
--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  console.log('===', date);
+  if(date === undefined){
+    return 'Unable to determine the time of year!';
+    //console.log('1--', date);
+  }  
+  if(!(date instanceof Date)){
+    return new Error('Invalid date!');
+    //console.log('2--', date);
+  }
+  let season = date.getMonth();
+  season == 11 ? season = 0 : season += 1; 
+  season = Math.trunc(season / 3);
+  //console.log(season);
+  if(season ==  0){return 'winter'};
+  if(season ==  1){return 'spring'};
+  if(season ==  2){return 'summer'};
+  if(season ==  3){return 'autumn'};
+
 }
 
 module.exports = {
   getSeason
 };
+
+ //console.log( getSeason(1));
+ //console.log(getSeason());
+ console.log(getSeason(new Date('1995-01-17T03:24:00')));
+ console.log(getSeason(new Date('1995-02-17T03:24:00')));
+ console.log(getSeason(new Date('1995-03-17T03:24:00')));
+ console.log(getSeason(new Date('1995-04-17T03:24:00')));
+ console.log(getSeason(new Date('1995-05-17T03:24:00')));
+ console.log(getSeason(new Date('1995-05-17T03:24:00')));
+ console.log(getSeason(new Date('1995-06-17T03:24:00')));
+ console.log(getSeason(new Date('1995-07-17T03:24:00')));
+ console.log(getSeason(new Date('1995-08-17T03:24:00')));
+ console.log(getSeason(new Date('1995-09-17T03:24:00')));
+ console.log(getSeason(new Date('1995-10-17T03:24:00')));
+ console.log(getSeason(new Date('1995-11-17T03:24:00')));
+ console.log(getSeason(new Date('1995-12-17T03:24:00')));
+ 
