44492f5acd99d32bb99ee56561fd3e2454531936
unknown <domogatskiy.a@yandex.ru>
2022-05-02 12:02:13 +0300

    complit task

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,11 +16,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  let elementSum = 0;
+  let zeroArrSave = []; 
+  for(let i = 0; i < matrix.length; i++){
+    let zeroArr = zeroArrSave.slice();
+    zeroArrSave = []; 
+    for(let j = 0; j < matrix[i].length; j++){
+      if (matrix[i][j] == 0){
+        zeroArrSave.push(j);
+      }
+      //console.log(j, !zeroArr.includes(j));
+      if(!zeroArr.includes(j)){
+        elementSum += matrix[i][j];
+      }
+    }
+  }
+  return elementSum;
 }
 
 module.exports = {
   getMatrixElementsSum
 };
+
+const matrix = [
+  [0, 1, 1, 2],
+  [0, 5, 0, 0],
+  [2, 0, 3, 3]
+ ];
+ 
+console.log( getMatrixElementsSum(matrix));
--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,11 +23,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let sweeper = [];
+  matrix.forEach(element => {
+    sweeper.push(element.slice().fill(0)); //заполняем поле нулями
+  }); 
+  //console.log(sweeper); 
+  for(let i = 0; i < matrix.length; i++ ){
+    for(let j = 0; j < matrix[i].length; j++){
+      if(matrix[i][j]){
+        if(i-1 >= 0             && j-1 >= 0               ){sweeper[i-1][j-1]++};
+        if(i-1 >= 0                                       ){sweeper[i-1][j  ]++};
+        if(i-1 >= 0             && j+1 <  matrix[i].length){sweeper[i-1][j+1]++};
+        if(                        j-1 >= 0               ){sweeper[i  ][j-1]++};
+        if(                        j+1 <  matrix[i].length){sweeper[i  ][j+1]++};
+        if(i+1 <  matrix.length && j-1 >= 0               ){sweeper[i+1][j-1]++};
+        if(i+1 <  matrix.length && j   >= 0               ){sweeper[i+1][j  ]++};
+        if(i+1 <  matrix.length && j+1 <  matrix[i].length){sweeper[i+1][j+1]++};
+        console.log(i,j,sweeper);
+      }
+    }
+  }
+  return sweeper;
 }
 
 module.exports = {
   minesweeper
 };
+
+const matrix = [
+  [true, false, false],
+  [false, true, false],
+  [false, false, false]
+ ];
+ 
+console.log( minesweeper(matrix));
